0be8e531be1071061add7e3a6ee39648e58345b2
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-17 02:46:30 -0700

    Setting up our React app

---
+++ client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "project1cmpe172"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://hrtm2qdvjl.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_wzB9vQASc",
+      APP_CLIENT_ID: "54d8j17pp4mq9o2dq8m5p5c3mv",
+      IDENTITY_POOL_ID: "us-west-2:2da7820c-2676-4af6-95d9-53423dd5ec8a"
+    }
+  };
