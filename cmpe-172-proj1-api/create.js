446e9c15e4113078a8f69e19adf2f4a45e205bd4
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-21 13:56:49 -0700

    Mistake testing

--- cmpe-172-proj1-api/create.js
+++ cmpe-172-proj1-api/create.js
@@ -3,7 +3,6 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  uuid.abc.gibberish;
   console.log(context);
   console.log(event);
   const data = JSON.parse(event.body);
