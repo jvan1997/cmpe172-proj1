b6afa5d07f9d73ec2c280dcc2d57ee61a6884db9
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-21 01:26:06 -0700

    Setting up our Serverless infrastructure

--- cmpe-172-proj1-api/create.js
+++ cmpe-172-proj1-api/create.js
@@ -3,15 +3,21 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+  console.log(context);
+  console.log(event);
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       nodeId: uuid.v1(),
+      firstname:data.firstname,
+      lastname:data.lastname,
       content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      attachment: data.file,
+      title:data.title,
+      createdAt: Date.now(),
+      updatedAt: Date.now()
     }
   };
 
@@ -19,6 +25,7 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
+    console.log(e);
     return failure({ status: false });
   }
 }
--- cmpe-172-proj1-api/update.js
+++ cmpe-172-proj1-api/update.js
@@ -3,25 +3,23 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
+  console.log(data);
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be updated
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       nodeId: event.pathParameters.id
     },
-    // 'UpdateExpression' defines the attributes to be updated
-    // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET content = :content, attachment = :attachment, firstname = :firstname, lastname = :lastname, title = :title, updatedAt = :updatedAt",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
-      ":content": data.content || null
+      ":attachment": data.file || null,
+      ":content": data.content || null,
+      ":firstname":data.firstname|| null,
+      ":lastname":data.lastname||null,
+      ":title":data.title||null,
+      ":updatedAt":Date.now()||null
     },
-    // 'ReturnValues' specifies if and how to return the item's attributes,
-    // where ALL_NEW returns all attributes of the item after the update; you
-    // can inspect 'result' below to see how it works with different settings
+
     ReturnValues: "ALL_NEW"
   };
 
